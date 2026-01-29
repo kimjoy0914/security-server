bcd347185843ee417c1f1d088c0893763555f72a
kimjoy0914 <kimjoy0914@gmail.com>
2026-01-29 21:48:27 +0900

    수정

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home page";
+        //return "home page";
+        return "home page 수정";
     }
 }
